d41f6afa57e17cad37d9d13754ed86368c8676b4
Steve Magoun <smagoun@gmail.com>
2020-05-06 12:51:45 +1200

    Add function to compare color values

--- color.js
+++ color.js
@@ -17,4 +17,14 @@ class Color {
     getName() {
         return this.name;
     }
+
+    /**
+     * Utility function to compare color values. Ignores alpha channel.
+     * 
+     * @param {Array} a RGB or RGBA color information
+     * @param {Array} b RGB or RGBA color information
+     */
+    static sameColor(a, b) {
+        return a[0] === b[0] && a[1] === b[1] && a[2] === b[2];
+    }
 }
