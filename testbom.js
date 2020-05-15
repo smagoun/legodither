e4813f5b25c7b19fbae67e5bd2fd425d87fee372
Steve Magoun <smagoun@gmail.com>
2020-05-15 22:34:49 +1200

    Test findOptimalBricks instead of findBestCostBricks

--- testbom.js
+++ testbom.js
@@ -1,8 +1,15 @@
 
-function sameBricks(a, b) {
+function checkBricks(a, b) {
     if (a.length === b.length) {
         for (let i = 0; i < a.length; i++) {
-            if (a[i] != b[i]) {
+            if (a[i] instanceof Brick && b[i] instanceof Brick) {
+                if (!a[i].isComplete() || !b[i].isComplete()) {
+                    return false;
+                }
+                if (!a[i].isEqual(b[i])) {
+                    return false;
+                }
+            } else if (a[i] != b[i]) {
                 return false;
             }
         }
@@ -11,14 +18,14 @@ function sameBricks(a, b) {
     return false;
 }   
 
-function testFindBestCostBricks(length, expectedCost, expectedBricks) {
+function testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks) {
     ret = true;
-    let {cost, bricks} = findBestCostBricks(length);
+    let {cost, bricks} = findOptimalBricks(x, y, length, color);
     if (cost != expectedCost) {
         console.error(`Length ${length} cost: ${cost}, expected: ${expectedCost}`);
         ret = false;
     }
-    if (!sameBricks(bricks, expectedBricks)) {
+    if (!checkBricks(bricks, expectedBricks)) {
         console.error(`Length ${length} got bricks ${bricks}, expected: ${expectedBricks}`);
         ret = false;
     }
@@ -32,39 +39,42 @@ function test1D() {
     let expectedCost;
     let expectedBricks = [];
     let length;
+    let color = [0, 0, 0, 0];
 
     // Base case: 0x0
     testCnt++;
-    length = 0, expectedCost = 0, expectedBricks = [0];
-    if (!testFindBestCostBricks(length, expectedCost, expectedBricks)) {
+    length=0, x=0, y=0, expectedCost=0, expectedBricks=[new Brick(length, 0, color, 0, 0)];
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
         errCnt++;
     }
 
     // 1x1
     testCnt++;
-    length = 1, expectedCost = 6, expectedBricks = [1];
-    if (!testFindBestCostBricks(length, expectedCost, expectedBricks)) {
+    length=1, x=0, y=0, expectedCost=6, expectedBricks=[new Brick(1, 6, color, 0, 0)];
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
         errCnt++;
     }
 
     // Longer brick
     testCnt++;
-    length = 4, expectedCost = 10, expectedBricks = [4];
-    if (!testFindBestCostBricks(length, expectedCost, expectedBricks)) {
+    length=4, x=0, y=0, expectedCost=10, expectedBricks=[new Brick(4, 10, color, 0, 0)];
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
         errCnt++;
     }
 
     // Composite
     testCnt++;
-    length = 5, expectedCost = 14, expectedBricks = [3,2];  // 2,3 would also work...
-    if (!testFindBestCostBricks(length, expectedCost, expectedBricks)) {
+    length=5, x=0, y=0, expectedCost=14, expectedBricks=[new Brick(3, 7, color, 0, 0),
+        new Brick(2, 7, color, 3, 0)];  // 2,3 would also work...
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
         errCnt++;
     }
 
     // Long run
     testCnt++;
-    length = 12, expectedCost = 27, expectedBricks = [8,4];  // 2,3 would also work...
-    if (!testFindBestCostBricks(length, expectedCost, expectedBricks)) {
+    length=12, x=0, y=0, expectedCost=27, expectedBricks=[new Brick(8, 17, color, 0, 0),
+        new Brick(4, 10, color, 8, 0)]; // 4,8 would also work...
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
         errCnt++;
     }
 
@@ -74,12 +84,21 @@ function test1D() {
     let tmpBCM = brickCostMap[4]; // Since it's a global we need to put it back the way we found it
     brickCost[4] = 16;
     delete brickCostMap[4];
-    length = 4, expectedCost = 13, expectedBricks = [3,1];
-    if (!testFindBestCostBricks(length, expectedCost, expectedBricks)) {
+    length=4, x=0, y=0, expectedCost=13, expectedBricks=[new Brick(3, 7, color, 0, 0),
+        new Brick(1, 6, color, 3, 0)]; // 1,3 would also work...
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
         errCnt++;
     }
     brickCost[4] = tmpBC;
     brickCostMap[4] = tmpBCM;
+
+    // Composite
+    testCnt++;
+    length=5, x=3, y=2, expectedCost=14, expectedBricks=[new Brick(3, 7, color, 3, 2),
+        new Brick(2, 7, color, 6, 2)];  // 2,3 would also work...
+    if (!testFindOptimalBricks(x, y, length, color, expectedCost, expectedBricks)) {
+        errCnt++;
+    }
 
     if (errCnt === 0) {
         console.log("1D-BOM tests passed!");
