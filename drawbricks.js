0d9e029758239fcfe0902cfb7c3421ae7f367ffc
Steve Magoun <smagoun@gmail.com>
2020-05-20 13:45:40 +1200

    Fix blurred lines in rendered bricks

--- drawbricks.js
+++ drawbricks.js
@@ -12,8 +12,11 @@ const INST_STUD_WIDTH = 10;
  * @param {*} height Height of the image in bricks
  */
 function drawBricks(bricks, canvas, width, height) {
-    const canvasWidth = width * INST_STUD_WIDTH;
-    const canvasHeight = height * INST_STUD_WIDTH;
+    // Off-by-0.5 / off-by-1 shenanigans are because strokes render from the pixel center; 
+    // doing it this way lets a 1px line fully-occupy a pixel instead of being blurred across 
+    // 2 pixels
+    const canvasWidth = width * INST_STUD_WIDTH + 1;
+    const canvasHeight = height * INST_STUD_WIDTH + 1;
     canvas.setAttribute("width", canvasWidth);
     canvas.setAttribute("height", canvasHeight);
     const ctx = canvas.getContext("2d");
@@ -22,6 +25,6 @@ function drawBricks(bricks, canvas, width, height) {
     for (const brick of bricks) {
         const x = brick.x * INST_STUD_WIDTH;
         const y = brick.y * INST_STUD_WIDTH;
-        ctx.strokeRect(x, y, brick.width * INST_STUD_WIDTH, brick.height * INST_STUD_WIDTH);
+        ctx.strokeRect(x + 0.5, y + 0.5, brick.width * INST_STUD_WIDTH, brick.height * INST_STUD_WIDTH);
     }
 }
