a51049446ef5236f8285d9d0b5e2aa685383e9a3
Steve Magoun <smagoun@gmail.com>
2021-01-02 12:27:43 -0500

    Add color names

--- palettes/palette-2bitgray.js
+++ palettes/palette-2bitgray.js
@@ -4,8 +4,8 @@ class Palette2BitGray extends Palette {
 
 Palette2BitGray.palette = [
     [[0, 0, 0], "Black"],
-    [[84, 84, 84], ],
-    [[169, 169, 169], ],
+    [[84, 84, 84], "Dark Gray"],
+    [[169, 169, 169], "Light Gray"],
     [[255, 255, 255], "White" ],
 ];
 
